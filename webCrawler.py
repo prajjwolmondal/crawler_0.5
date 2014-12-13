871673b44370d5fca410ceea43ff4cb16bd4026e
theposter <prajj.mondal@gmail.com>
2014-12-13 01:49:18 -0500

    Delete webCrawler.py

--- webCrawler.py
+++
@@ -1,7 +0,0 @@
-import urllib
-import pprint
-
-#raw_input is the basic way of taking input from user
-input = raw_input('Enter website to be crawled: ');
-htmlText = urllib.urlopen(input).read();
-print htmlText;
